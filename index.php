6214ce098f7b79ca3cec225a991fbde3b2ec69b3
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-09-29 13:09:33 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$var=4;
+$var3=30;
+$var2="Hi, This Is Practice File.";
+
+echo $var+$var3."<p>".$var2;
+
+
+$newvar="<br> This Is THe New Lesson For Practice.";
+$check=3;
+if($check==5){
+    echo  $newvar;
+}
+elseif ($check>=4){
+    echo "<p>Second Result";
+}
+else {
+    echo " <br>Third Result";
+}
+?>
