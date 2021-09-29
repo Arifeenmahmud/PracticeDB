0a96ec2499d63c8f954b22f7f33e2d757673f050
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-09-29 18:59:05 +0600

    Show Data From Database

--- DBConnection.php
+++ DBConnection.php
@@ -5,5 +5,15 @@ if ($conn->connect_error) {
  }
    echo "Connected successfully";
    //$conn->query("INSERT INTO name set ID='08', First_nane='Jabbar',Last_name='hossin'");
-
+   
+   $data=mysqli_query($conn,"SELECT `ID`,`First_nane`,`Last_name` FROM `name`");
+   echo "<center><table border=1>";
+   while($row=mysqli_fetch_assoc($data)){
+   echo "<tr>";
+       echo "<td>".$row['ID']."</td>".
+            "<td>".$row['First_nane']."</td>".
+            "<td>".$row['Last_name']."</td>";
+   echo "</tr>";
+   }
+   echo "</table></center>";
 ?>
